fb3ddec8c2be3bc591ce5c7fa23b06d5ba1ac6bb
Anna Sniegula <anna.sniegula@gmail.com>
2021-06-28 21:56:29 +0200

    className in Container unit changed

--- src/Container/index.js
+++ src/Container/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Container = ({children}) => (
     <main className="container">
         {children}
-        <a className="main__link" rel="noreferrer noopener" target="_blank"
+        <a className="container__link" rel="noreferrer noopener" target="_blank"
           href="https://www.nbp.pl/home.aspx?f=/statystyka/kursy.html">
           Aktualne kursy walut NBP </a>
     </main>
