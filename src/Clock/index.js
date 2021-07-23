82b7bc337acf3440d496d85fa0ab6dc6dd4ce6eb
Anna Sniegula <anna.sniegula@gmail.com>
2021-07-23 20:56:04 +0200

    Hook useCurrentDate added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,34 +1,14 @@
-import { useEffect, useState } from "react";
+import { useCurrentDate } from "./useCurrentDate";
 import { StyledClock } from "./styled";
 
 const Clock = () => {
-    const [date, setDate] = useState(new Date());
-
-    const formattedDate = date.toLocaleString(undefined, {
-        day: "numeric",
-        weekday: "long",
-        month: "long",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-    });
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    const date = useCurrentDate();
 
     return (
         <StyledClock>
             Dzisiaj jest
             {" "}
-            {formattedDate}
+            {date}
         </StyledClock>
     )
 };
---
+++ src/Clock/useCurrentDate.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export const useCurrentDate = () => {
+    const [date, setDate] = useState(new Date());
+
+    const formattedDate = date.toLocaleString(undefined, {
+        day: "numeric",
+        weekday: "long",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    });
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return formattedDate;
+};
